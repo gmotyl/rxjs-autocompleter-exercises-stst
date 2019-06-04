9e9a1fa2223a2a9b34723b0295eacd63ab22c76a
gom3s <gmotyl@gmail.com>
2019-06-04 10:05:49 +0200

    refactored to one subscription

--- index.ts
+++ index.ts
@@ -1,23 +1,34 @@
 import { names, getNames } from './data'
 import * as dom from './dom'
 
-import { Observable, Observer, fromEvent } from 'rxjs'
+import { Observable, Observer, fromEvent, merge } from 'rxjs'
 // import { of } from 'rxjs'; 
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap, share } from 'rxjs/operators';
 
 const phrase$ = fromEvent(dom.$input, 'input').pipe(
   map(e => (e.target as HTMLInputElement).value),
+  tap(console.log),
+  share(),
 )
 const searchResults$ = phrase$.pipe(
   filter(phrase => phrase.length > 2),
   map(getNames),
-  map(results => results.slice(0, 10))
+  map(results => results.slice(0, 10)),
+  tap(dom.renderList),
 );
 
-searchResults$.subscribe(dom.renderList);
+// searchResults$.subscribe(dom.renderList);
 
 const searchClear$ = phrase$.pipe(
-  filter(phrase => phrase.length <= 2)
+  filter(phrase => phrase.length <= 2),
+  tap(dom.clearList),
 )
 
-searchClear$.subscribe(dom.clearList);
+const autocomplete$ = merge(
+  searchResults$, 
+  searchClear$
+)
+
+autocomplete$.subscribe();
+
+// searchClear$.subscribe(dom.clearList);
