a994de9d0642c5eeb06f29d91777a16e550a365d
gom3s <gmotyl@gmail.com>
2019-06-04 11:42:19 +0200

    employeesByName refactored to operator (static pipe)

--- model.ts
+++ model.ts
@@ -1,15 +1,24 @@
 const baseURL = 'http://localhost:3000'
 
 import { ajax } from 'rxjs/ajax'
-import { publishLast, refCount } from 'rxjs/operators'
+import { publishLast, publishReplay refCount, switchMap, shareReplay } from 'rxjs/operators'
+import { pipe } from 'rxjs'
 
 export const employees$ = ajax.getJSON(`${baseURL}/employees`).pipe(
   publishLast(),
   refCount(),
 )
 
-export const employeesByName = 
+export const __employeesByName = 
   (firstName: string) => ajax.getJSON(`${baseURL}/employees?firstName_like=${firstName}`).pipe(
     publishLast(),
     refCount(),
+  )
+
+export const employeesByName = () =>
+  pipe(
+    switchMap((phrase: string) => ajax.getJSON(`${baseURL}/employees?firstName_like=${phrase}`)),
+    publishReplay(1),
+    refCount(),
+    // shareReplay(1),
   )
