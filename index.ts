b056932b969514bf37fc67086020c7d6a5da9ffc
gom3s <gmotyl@gmail.com>
2019-06-04 13:28:50 +0200

    switchMap searchResult VS clear

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 import { names, getNames } from './data'
 import * as dom from './dom'
 
-import { Observable, Observer, fromEvent, merge } from 'rxjs'
+import { Observable, Observer, fromEvent, merge} from 'rxjs'
 // import { of } from 'rxjs'; 
-import { map, filter, tap, share, distinctUntilChanged } from 'rxjs/operators';
+import { map, filter, tap, shareReplay, distinctUntilChanged, switchMap  } from 'rxjs/operators';
 
 import { employeesByName } from './model'
 
-const phrase$ = fromEvent(dom.$input, 'input').pipe(
+const phrase$: Observable<string> = fromEvent(dom.$input, 'input').pipe(
   map(e => (e.target as HTMLInputElement).value),
   distinctUntilChanged(),
   tap(console.log),
-  share(),
+  shareReplay(1),
 )
 const searchResults$ = phrase$.pipe(
   filter(phrase => phrase.length > 2),
@@ -29,13 +29,19 @@ const searchResults$ = phrase$.pipe(
 // searchResults$.subscribe(dom.renderList);
 
 const searchClear$ = phrase$.pipe(
+  tap((p) => console.log(p, 'CLEAR')),
   filter(phrase => phrase.length <= 2),
   tap(dom.clearList),
 )
 
-const autocomplete$ = merge(
-  searchResults$, 
-  searchClear$
+const autocomplete$ = phrase$.pipe(
+  tap(p => console.log(p, 'PRZED')),
+  
+  switchMap((phrase) => phrase.length > 2 
+    ? searchResults$
+    : searchClear$
+  ),
+  tap(p => console.log(p, 'PO')),
 )
 
 autocomplete$.subscribe();
